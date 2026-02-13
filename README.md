807913653881f9bdff3c6208a37b8f0b7efe502a
idasu <idasuozdemir@gmail.com>
2026-02-13 09:59:13 -0500

    Setup Junit and Maven for Testing

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ECSE429-Project
+
+### Tips to Run
+
+- Helpful to use `@BeforeAll` method in JUnit to ensure the server is responding before tests start
+
+- Run `mvn test` on terminal in root (where pom.xml is) or just run
---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,17 @@
+import static io.restassured.RestAssured.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestDemo {
+    @Test
+    @DisplayName("Check if the Todo Manager API is running")
+    void testApiIsUp() {
+        // Most Todo Manager APIs run on port 4567 or 8080 by default
+        given()
+            .baseUri("http://localhost:4567")
+        .when()
+            .get("/todos")
+        .then()
+            .statusCode(200); // If this fails, your JAR isn't running!
+    }
+}
