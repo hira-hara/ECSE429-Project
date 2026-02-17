68d10d0b819578d669df16e057fe8e4671c12aff
idasu <idasuozdemir@gmail.com>
2026-02-17 11:29:22 -0500

    Finished JSON parts

--- src/test/java/TestProjectJSONDoc.java
+++ src/test/java/TestProjectJSONDoc.java
@@ -27,20 +27,22 @@ public class TestProjectJSONDoc {
     }
 
     @BeforeEach
-    void setUp() { // Set up initial conditions and save system state
+    void setUp() {
         Response response = given()
                 .contentType(ContentType.JSON)
-                .body("{\"title\":\"Initial Project\", \"description\":\"Setup state\"}")
-                .post("/projects");
+                .when()
+                .get("/projects");
+        
+        testProjectId = response.jsonPath().getString(("projects[-1].id"));
 
-        testProjectId = response.jsonPath().getString("id");
-    }
-
-    @AfterEach
-    void tearDown() { // Restore system to initial state
-        if (testProjectId != null) {
-            given().delete("/projects/" + testProjectId);
+        if (testProjectId == null) {
+            Response createResponse = given()
+                    .contentType(ContentType.JSON)
+                    .body("{\"title\":\"Initial Project\"}")
+                    .post("/projects");
+            testProjectId = createResponse.jsonPath().getString("id");
         }
+
     }
 
     /* JSON TESTS */
@@ -64,17 +66,17 @@ public class TestProjectJSONDoc {
     @DisplayName("POST /projects JSON")
     void testCreateProject() {
         Response response = given()
-            .contentType(ContentType.JSON)
-            .body("{\"title\":\"New Project\", \"completed\":false, \"description\":\"new project to do\"}")
-        .when()
-            .post("/projects");
+                .contentType(ContentType.JSON)
+                .body("{\"title\":\"New Project\", \"completed\":false, \"description\":\"new project to do\"}")
+                .when()
+                .post("/projects");
 
         String newId = response.jsonPath().getString("id"); // Capture id for immediate cleanup
 
         try {
             response.then()
-                .statusCode(201)
-                .body("title", equalTo("New Project"));
+                    .statusCode(201)
+                    .body("title", equalTo("New Project"));
         } finally { // Clean up to return to state
             if (newId != null) {
                 given().delete("/projects/" + newId);
@@ -97,14 +99,13 @@ public class TestProjectJSONDoc {
                 .body("projects[0].id", equalTo(testProjectId));
     }
 
-
     @Test
     @DisplayName("POST /projects/:id JSON")
     void testCreateUpdateDeleteFlow() {
         Response createResponse = given() // New project acting as old one
                 .contentType(ContentType.JSON)
                 .body("{\"title\":\"Old Project\", \"completed\":false}")
-            .when()
+                .when()
                 .post("/projects");
 
         createResponse.then().statusCode(201);
@@ -112,44 +113,114 @@ public class TestProjectJSONDoc {
 
         try { // Update parameters
             given()
-                .contentType(ContentType.JSON)
-                .body("{\"title\":\"Done Project\", \"completed\":true}")
-            .when()
-                .post("/projects/" + localId) 
-            .then()
-                .log().all()
-                .statusCode(200) 
-                .body("title", equalTo("Done Project"))
-                .body("completed", equalTo("true"));
+                    .contentType(ContentType.JSON)
+                    .body("{\"title\":\"Done Project\", \"completed\":true}")
+                    .when()
+                    .post("/projects/" + localId)
+                    .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("title", equalTo("Done Project"))
+                    .body("completed", equalTo("true"));
 
         } finally {
             // Delete project
             if (localId != null) {
                 given()
-                    .delete("/projects/" + localId)
-                .then()
-                    .statusCode(200); 
+                        .delete("/projects/" + localId)
+                        .then()
+                        .statusCode(200);
             }
         }
     }
 
+    // Endpoint: /projects/:id/tasks
+    @Test
+    @DisplayName("GET /projects/:id/tasks JSON")
+    void testGetProjectIDTasks() {
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("/projects/" + testProjectId + "/tasks")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("size()", greaterThan(0))
+                .body("todos", hasSize(greaterThan(0)));
+    }
+
+    @Test
+    @DisplayName("POST /projects/:id/tasks JSON")
+    void testPostProjectTasks() {
+        Response createResponse = given() // New project acting as old one
+                .contentType(ContentType.JSON)
+                .body("{\"title\":\"Old Project\", \"completed\":false}")
+                .when()
+                .post("/projects");
+
+        createResponse.then().statusCode(201);
+        String localId = createResponse.jsonPath().getString("id");
+
+        try { // Update parameters
+            given()
+                    .contentType(ContentType.JSON)
+                    .body("{\"id\":\"1\"}")
+                    .when()
+                    .post("/projects/" + localId + "/tasks")
+                    .then()
+                    .log().all()
+                    .statusCode(201);
+
+        } finally {
+            // Delete project
+            if (localId != null) {
+                given()
+                        .delete("/projects/" + localId)
+                        .then()
+                        .statusCode(200);
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("HEAD /projects/:id/tasks JSON")
+    void testHeadProjectIDTasks() {
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                .head("/projects/" + testProjectId + "/tasks")
+                .then()
+                .statusCode(200);
+    }
+
     // TESTING UNEXPECTED INPUTS
     @Test
-    @DisplayName("DELETE /projects/:id JSON")
+    @DisplayName("DELETE /projects/:id NON EXISTENT ID")
     void testDeleteProjectIDErr() {
         given()
+                .accept(ContentType.JSON)
+                .when()
+                .delete("/projects/" + 40000)
+                .then()
+                .statusCode(404)
+                .contentType(ContentType.JSON)
+                .body("errorMessages[0]", equalTo("Could not find any instances with projects/40000"));
+    }
+
+    @Test
+    @DisplayName("POST /projects/:id UNDEFINED INPUT")
+    void testPostProjectErr() {
+        given()
+            .contentType(ContentType.JSON)
             .accept(ContentType.JSON)
             .when()
-            .delete("/projects/" + 40000)
+            .post("/projects/" + "hello")
             .then()
+            .log().all()
             .statusCode(404)
             .contentType(ContentType.JSON)
-            .body("errorMessages[0]", equalTo("Could not find any instances with projects/40000"));
+            .body("errorMessages[0]", containsString("No such project entity instance with GUID or ID hello found"));
     }
 
 
-
-    // test a post /projects/undefined or null
-
 }
-
